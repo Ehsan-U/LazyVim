d726242612b5f779011fb55d4bb3d68b14cbe693
Ehsan U. <88642086+Ehsan-U@users.noreply.github.com>
2024-11-23 20:16:04 +0500

    rm snippets coding.lua

--- lua/lazyvim/plugins/coding.lua
+++ lua/lazyvim/plugins/coding.lua
@@ -87,29 +87,29 @@ return {
   },
 
   -- snippets
-  {
-    "nvim-cmp",
-    optional = true,
-    dependencies = {
-      {
-        "garymjr/nvim-snippets",
-        opts = {
-          friendly_snippets = true,
-        },
-        dependencies = { "rafamadriz/friendly-snippets" },
-      },
-    },
-    opts = function(_, opts)
-      opts.snippet = {
-        expand = function(item)
-          return LazyVim.cmp.expand(item.body)
-        end,
-      }
-      if LazyVim.has("nvim-snippets") then
-        table.insert(opts.sources, { name = "snippets" })
-      end
-    end,
-  },
+  -- {
+  --   "nvim-cmp",
+  --   optional = true,
+  --   dependencies = {
+  --     {
+  --       "garymjr/nvim-snippets",
+  --       opts = {
+  --         friendly_snippets = true,
+  --       },
+  --       dependencies = { "rafamadriz/friendly-snippets" },
+  --     },
+  --   },
+  --   opts = function(_, opts)
+  --     opts.snippet = {
+  --       expand = function(item)
+  --         return LazyVim.cmp.expand(item.body)
+  --       end,
+  --     }
+  --     if LazyVim.has("nvim-snippets") then
+  --       table.insert(opts.sources, { name = "snippets" })
+  --     end
+  --   end,
+  -- },
 
   -- auto pairs
   {
